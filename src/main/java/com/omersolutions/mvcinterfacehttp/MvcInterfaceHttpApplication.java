b4d9ca51fd0a07f13a7ec810259b1d47ba941a41
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-05-19 17:40:28 -0500

    Initial commit

---
+++ src/main/java/com/omersolutions/mvcinterfacehttp/MvcInterfaceHttpApplication.java
@@ -0,0 +1,13 @@
+package com.omersolutions.mvcinterfacehttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcInterfaceHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcInterfaceHttpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omersolutions/mvcinterfacehttp/MvcInterfaceHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omersolutions.mvcinterfacehttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcInterfaceHttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
